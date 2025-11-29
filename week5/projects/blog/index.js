ebf9cc302c536178bf670d10c390f0794c8249cc
Raji Mahmud <raji@mahmud.dev>
2025-11-29 15:47:19 +0100

    added standard logger middleware

--- week5/projects/blog/index.js
+++ week5/projects/blog/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import postRoutes from './routes/posts.routes.js';
 import {loadEnvFile} from 'node:process';
+import morgan from "morgan"
+import { randomUUID } from 'crypto';
 loadEnvFile()
+
 const s = express()
 const PORT = process.env.PORT
+
+s.use(function assignId (req, res, next) {req.id = randomUUID(); next()})
+morgan.token('id', function getId(req) { return req.id});
+s.use(morgan(":id :method :url :status :response-time ms - :res[content-length]"))
 s.use("/api/posts", postRoutes)
 s.use('/static', express.static("./static"))
 
